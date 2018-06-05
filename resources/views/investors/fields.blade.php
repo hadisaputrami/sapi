0a450a4e69c1fdc7349c418d36462cc6fbf88e2e
HD-PC\HD <hadisaputrami@gmail.com>
2018-06-05 11:07:25 -0700

    update

--- resources/views/investors/fields.blade.php
+++ resources/views/investors/fields.blade.php
@@ -19,9 +19,12 @@
 </div>
 
 <!-- Users Id Field -->
-    {!! Form::hidden('users_id', Auth::id(),null, ['class' => 'form-control']) !!}
+    {!! Form::hidden('users_id', $nama,null, ['class' => 'form-control']) !!}
 
-
+<div class="form-group col-sm-6">
+    {!! Form::label('users_id', 'Users Id:') !!}
+    {!! Form::select('users_id',$nama, null, ['class' => 'form-control']) !!}
+</div>
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
     {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
