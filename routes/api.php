a61b9ebbfb340819e8abb5fc03ef65ffef385ced
DESKTOP-BJIVU7J\Asus-PC <srihhandayanii@gmail.com>
2018-08-17 17:10:39 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,12 @@ Route::group(['middleware' => 'jwt.auth'], function () {
     Route::post('users/update','UserAPIController@update');
     Route::post('users/password','UserAPIController@changePassword');
     Route::post('token_device','UserAPIController@storeTokenDevice');
+    Route::resource('investor_has_transaksi_investasis', 'InvestorHasTransaksiInvestasiAPIController');
+    
+Route::resource('trans_inves_has_status_inves', 'TransInvesHasStatusInvesAPIController');
+
 });
+
 Route::post('users','UserAPIController@store');
 Route::post('token','AuthenticateAPIController@authenticate');
 Route::post('reset_password', 'UserAPIController@resetPassword');
@@ -41,31 +46,11 @@ Route::resource('biodatas', 'BiodataAPIController');
 
 Route::resource('konfirmasi_investors', 'KonfirmasiInvestorAPIController');
 
-Route::resource('konfirmasi_investors', 'KonfirmasiInvestorAPIController');
-
-Route::resource('konfirmasi_investors', 'KonfirmasiInvestorAPIController');
-
-Route::resource('paket_investasis', 'PaketInvestasiAPIController');
-
-Route::resource('transaksi_investasis', 'TransaksiInvestasiAPIController');
-
-Route::resource('investor_has_transaksi_investasis', 'InvestorHasTransaksiInvestasiAPIController');
-
 Route::resource('investors', 'InvestorAPIController');
-
-Route::resource('ternak_investasis', 'TernakInvestasiAPIController');
-
-Route::resource('transaksi_investasis', 'TransaksiInvestasiAPIController');
-
-Route::resource('trans_penj_has_status_penj', 'TransaksiPenjualanHasStatusPenjualanAPIController');
-
-Route::resource('trans_inves_has_status_inves', 'TransInvesHasStatusInvesAPIController');
 
 Route::resource('transaksi_investasis', 'TransaksiInvestasiAPIController');
 
 Route::resource('asuransis', 'AsuransiAPIController');
-
-Route::resource('jenis_pembayarans', 'JenisPembayaranAPIController');
 
 Route::resource('jenis_pembayarans', 'JenisPembayaranAPIController');
 
@@ -85,6 +70,5 @@ Route::resource('ternak_non_investasis', 'TernakNonInvestasiAPIController');
 
 Route::resource('progres', 'ProgresAPIController');
 
-Route::resource('trans_inves_has_status_inves', 'TransInvesHasStatusInvesAPIController');
 
 Route::resource('trans_penj_has_status_penj', 'TransaksiPenjualanHasStatusPenjualanAPIController');
